d552bdb4e169b5a71ce4014a0fdd5c015c833e95
Geefrank247 <162192270+Geefrank247@users.noreply.github.com>
2025-01-05 01:56:37 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,341 @@
+
+
+        <!-- =============== START OF FOOTER =============== -->
+        <footer class="footer1 bg-dark">
+
+            <!-- ===== START OF FOOTER WIDGET AREA ===== -->
+            <div class="footer-widget-area ptb100">
+                <div class="container">
+                    <div class="row">
+
+                        <!-- Start of Widget 1 -->
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="widget widget-about">
+
+                                <!-- INSERT YOUR LOGO HERE -->
+                                <img src="assets/images/logo.svg" alt="logo" class="logo">
+                                <!-- INSERT YOUR WHITE LOGO HERE -->
+                                <img src="assets/images/logo-white.svg" alt="white logo" class="logo-white">
+                                <p class="nomargin">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, ducimus, atque. Praesentium suscipit provident explicabo dignissimos nostrum numquam deserunt earum accusantium et fugit.</p>
+                            </div>
+                        </div>
+                        <!-- End of Widget 1 -->
+
+                        <!-- Start of Widget 2 -->
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="widget widget-links">
+                                <h4 class="widget-title">Useful links</h4>
+
+                                <ul class="general-listing">
+                                    <li><a href="#">about movify</a></li>
+                                    <li><a href="#">blog</a></li>
+                                    <li><a href="#">forum</a></li>
+                                    <li><a href="#">my account</a></li>
+                                    <li><a href="#">watch list</a></li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <!-- End of Widget 2 -->
+
+                        <!-- Start of Widget 3 -->
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="widget widget-blog">
+                                <h4 class="widget-title">latest news</h4>
+
+                                <ul class="blog-posts">
+                                    <li><a href="#">blog post 1</a><small>januar 13, 2018</small></li>
+                                    <li><a href="#">blog post 2</a><small>januar 13, 2018</small></li>
+                                    <li><a href="#">blog post 3</a><small>januar 13, 2018</small></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- End of Widget 3 -->
+
+                        <!-- Start of Widget 4 -->
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="widget widget-social">
+                                <h4 class="widget-title">follow us</h4>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, ducimus, atque.</p>
+
+                                <!-- Start of Social Buttons -->
+                                <ul class="social-btns">
+                                    <!-- Social Media -->
+                                    <li>
+                                        <a href="#" class="social-btn-roll facebook">
+                                            <div class="social-btn-roll-icons">
+                                                <i class="social-btn-roll-icon fa fa-facebook"></i>
+                                                <i class="social-btn-roll-icon fa fa-facebook"></i>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <!-- Social Media -->
+                                    <li>
+                                        <a href="#" class="social-btn-roll twitter">
+                                            <div class="social-btn-roll-icons">
+                                                <i class="social-btn-roll-icon fa fa-twitter"></i>
+                                                <i class="social-btn-roll-icon fa fa-twitter"></i>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <!-- Social Media -->
+                                    <li>
+                                        <a href="#" class="social-btn-roll google-plus">
+                                            <div class="social-btn-roll-icons">
+                                                <i class="social-btn-roll-icon fa fa-google-plus"></i>
+                                                <i class="social-btn-roll-icon fa fa-google-plus"></i>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <!-- Social Media -->
+                                    <li>
+                                        <a href="#" class="social-btn-roll instagram">
+                                            <div class="social-btn-roll-icons">
+                                                <i class="social-btn-roll-icon fa fa-instagram"></i>
+                                                <i class="social-btn-roll-icon fa fa-instagram"></i>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <!-- End of Social Buttons -->
+
+                            </div>
+                        </div>
+                        <!-- End of Widget 4 -->
+
+                    </div>
+                </div>
+            </div>
+            <!-- ===== END OF FOOTER WIDGET AREA ===== -->
+
+
+            <!-- ===== START OF FOOTER COPYRIGHT AREA ===== -->
+            <div class="footer-copyright-area ptb30">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="d-flex">
+                                <div class="links">
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item"><a href="#">Privacy & Cookies</a></li>
+                                        <li class="list-inline-item"><a href="#">Terms & Conditions</a></li>
+                                        <li class="list-inline-item"><a href="#">Legal Disclaimer</a></li>
+                                        <li class="list-inline-item"><a href="#">Community</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="copyright ml-auto">All Rights Reserved by <a href="#">Movify</a>.</div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ===== END OF FOOTER COPYRIGHT AREA ===== -->
+
+        </footer>
+        <!-- =============== END OF FOOTER =============== -->
+
+    </div>
+    <!-- =============== END OF WRAPPER =============== -->
+
+
+
+
+    <!-- =============== START OF GENERAL SEARCH WRAPPER =============== -->
+    <div class="general-search-wrapper">
+        <form class="general-search" role="search" method="get" action="#">
+            <input type="text" placeholder="Type and hit enter...">
+            <span id="general-search-close" class="ti-close toggle-search"></span>
+        </form>
+    </div>
+    <!-- =============== END OF GENERAL SEARCH WRAPPER =============== -->
+
+
+
+    <!-- =============== START OF LOGIN & REGISTER POPUP =============== -->
+    <div id="login-register-popup" class="small-dialog zoom-anim-dialog mfp-hide">
+
+        <!-- ===== Start of Signin wrapper ===== -->
+        <div class="signin-wrapper">
+            <div class="small-dialog-headline">
+                <h4 class="text-center">Sign in</h4>
+            </div>
+
+
+            <div class="small-dialog-content">
+
+                <!-- Start of Login form -->
+                <form id="cariera_login" method="post">
+                    <p class="status"></p>
+
+                    <div class="form-group">
+                        <label for="username">Username or Email *</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Your Username or Email *" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password *</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Your Password *" />
+                    </div>
+
+                    <div class="form-group">
+                        <div class="checkbox pad-bottom-10">
+                            <input id="check1" type="checkbox" name="remember" value="yes">
+                            <label for="check1">Keep me signed in</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="Sign in" class="btn btn-main btn-effect nomargin" />
+                    </div>
+                </form>
+                <!-- End of Login form -->
+
+                <div class="bottom-links">
+                    <span>
+                        Not a member?
+                        <a  class="signUpClick">Sign up</a>
+                    </span>
+                    <a  class="forgetPasswordClick pull-right">Forgot Password</a>
+                </div>
+            </div>
+
+        </div>
+        <!-- ===== End of Signin wrapper ===== -->
+
+
+
+        <!-- ===== Start of Signup wrapper ===== -->
+        <div class="signup-wrapper">
+            <div class="small-dialog-headline">
+                <h4 class="text-center">Sign Up</h4>
+            </div>
+
+            <div class="small-dialog-content">
+
+                <!-- Start of Registration form -->
+                <form id="cariera_registration" action="#" method="POST">
+                    <p class="status"></p>
+
+                    <div class="form-group">
+                        <label for="movify_user_login">Username</label>
+                        <input name="movify_user_login" id="movify_user_login" class="form-control" type="text"/>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="movify_user_email">Email</label>
+                        <input name="movify_user_email" id="movify_user_email" class="form-control" type="email"/>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input name="movify_user_pass" id="movify_password" class="form-control" type="password"/>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-main btn-effect nomargin" value="Register"/>
+                    </div>
+                </form>
+                <!-- End of Registration form -->
+
+                <div class="bottom-links">
+                    <span>
+                        Already have an account?
+                        <a class="signInClick">Sign in</a>
+                    </span>
+
+                    <a class="forgetPasswordClick pull-right">Forgot Password</a>
+                </div>
+
+            </div> <!-- .small-dialog-content -->
+
+        </div>
+        <!-- ===== End of Signup wrapper ===== -->
+
+
+
+        <!-- ===== Start of Forget Password wrapper ===== -->
+        <div class="forgetpassword-wrapper">
+            <div class="small-dialog-headline">
+                <h4 class="text-center">Forgotten Password</h4>
+            </div>
+
+            <div class="small-dialog-content">
+
+                <!-- Start of Forger Password form -->
+                <form id="forget_pass_form" action="#"  method="post">
+                    <p class="status"></p>
+
+                    <div class="form-group">
+                        <label for="password">Email Address *</label>
+                        <input type="email" name="user_login" class="form-control" id="email3" placeholder="Email Address *" />
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" name="submit" value="Get New Password" class="btn btn-main btn-effect nomargin" />
+                    </div>
+                </form>
+                <!-- End of Forger Password form -->
+
+                <div class="bottom-links">
+                    <a class="cancelClick">Cancel</a>
+                </div>
+
+            </div><!-- .small-dialog-content -->
+
+        </div>
+        <!-- ===== End of Forget Password wrapper ===== -->
+
+    </div>
+    <!-- =============== END OF LOGIN & REGISTER POPUP =============== -->
+
+
+
+    <!-- ===== Start of Back to Top Button ===== -->
+    <div id="backtotop">
+        <a href="#"></a>
+    </div>
+    <!-- ===== End of Back to Top Button ===== -->
+
+
+
+    <!-- ===== All Javascript at the bottom of the page for faster page loading ===== -->
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.ajaxchimp.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.mmenu.js"></script>
+    <script src="assets/js/jquery.inview.min.js"></script>
+    <script src="assets/js/jquery.countTo.min.js"></script>
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/headroom.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+    <!-- ===== Slider Revolution core JavaScript files ===== -->
+    <script type="text/javascript" src="assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- ===== Slider Revolution extension scripts ===== -->
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+
+</body>
+
+
+<!-- Mirrored from gnodesign.com/templates/movify/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 05 Sep 2024 17:13:13 GMT -->
+</html>
---
+++ index.php
@@ -0,0 +1,179 @@
+
+<?php include "header.php"; ?>
+
+
+        <!-- =============== START OF SLIDER REVOLUTION SECTION =============== -->
+        <section id="slider" class="full-slider">
+            <div class="rev-slider-wrapper fullscreen-container overlay-gradient">
+                <div id="fullscreen-slider" class="rev_slider fullscreenbanner" style="display:none" data-version="5.4.1">
+                    <ul>
+
+                        <!-- ===== SLIDE NR. 1 ===== -->
+                        <li data-transition="fade">
+                            <!-- MAIN IMAGE -->
+                            <img src="assets/images/slider/tushslider1.jpg"
+                                 alt="Image"
+                                 title="slider-bg"
+                                 data-bgposition="center center"
+                                 data-bgfit="cover"
+                                 data-bgrepeat="no-repeat"
+                                 data-bgparallax="10"
+                                 class="rev-slidebg"
+                                 data-no-retina="">
+                            <!-- LAYER NR. 1 -->
+                            <div class="tp-caption tp-resizeme"
+                                 data-x="center"
+                                 data-hoffset=""
+                                 data-y="middle"
+                                 data-voffset="['-30','-30','-30','-30']"
+                                 data-responsive_offset="on"
+                                 data-fontsize="['60','50','40','30']"
+                                 data-lineheight="['60','50','40','30']"
+                                 data-whitespace="nowrap"
+                                 data-frames='[{"delay":1000,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                                 style="z-index: 5; color: #fff; font-weight: 900;">AKWA IBOM STATE BRASS
+                            </div>
+                        </li>
+
+                        <!-- ===== SLIDE NR. 2 ===== -->
+                        <li data-transition="fade"
+                            data-slotamount="7"
+                            data-easein="default"
+                            data-easeout="default"
+                            data-masterspeed="2000">
+                            <!-- MAIN IMAGE -->
+                            <img src="assets/images/slider/tushslider2.jpg"
+                                 alt="Image"
+                                 title="slider-bg"
+                                 data-bgposition="center top"
+                                 data-bgfit="cover"
+                                 data-bgrepeat="no-repeat"
+                                 data-bgparallax="10"
+                                 class="rev-slidebg"
+                                 data-no-retina="">
+                            <!-- LAYER NR. 1 -->
+                            <div class="tp-caption tp-resizeme"
+                                 data-x="center"
+                                 data-hoffset=""
+                                 data-y="middle"
+                                 data-voffset="['-30','-30','-30','-30']"
+                                 data-responsive_offset="on"
+                                 data-fontsize="['60','50','40','30']"
+                                 data-lineheight="['60','50','40','30']"
+                                 data-whitespace="nowrap"
+                                 data-frames='[{"delay":1000,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                                 style="z-index: 5; color: #fff; font-weight: 900;">AKWA IBOM STATE BRASS FORUM
+                            </div>
+                        </li>
+
+                        <!-- ===== SLIDE NR. 3 ===== -->
+                        <li data-transition="fade"
+                            data-slotamount="7"
+                            data-easein="default"
+                            data-easeout="default"
+                            data-masterspeed="2000">
+                            <!-- MAIN IMAGE -->
+                            <img src="assets/images/slider/tushslider3.jpg"
+                                 alt="Image"
+                                 data-bgposition="center top"
+                                 data-bgfit="cover"
+                                 data-bgrepeat="no-repeat"
+                                 data-bgparallax="3"
+                                 class="rev-slidebg"
+                                 data-no-retina>
+                            <!-- LAYER NR. 1 -->
+                            <div class="tp-caption tp-resizeme"
+                                 data-x="center"
+                                 data-hoffset=""
+                                 data-y="middle"
+                                 data-voffset="['-30','-30','-30','-30']"
+                                 data-responsive_offset="on"
+                                 data-fontsize="['60','50','40','30']"
+                                 data-lineheight="['60','50','40','30']"
+                                 data-whitespace="nowrap"
+                                 data-frames='[{"delay":1000,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                                 style="z-index: 5; color: #fff; font-weight: 900;">100% RELIABLE
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            <!-- ===== END OF REV SLIDER WRAPPER ===== -->
+        </section>
+
+
+        <!-- =============== END OF SUBSCRIBE SECTION =============== -->
+        <section class="subscribe ptb100">
+            <div class="container">
+
+               
+
+  <!-- Start of Contact Form -->
+  <div class="col-md-12 col-sm-12">
+                        <h3 class="title">Fill In Personal Information</h3>
+
+                        <!-- Start of Contact Form -->
+                        <form id="contact-form">
+
+                            <!-- contact result -->
+                            <div id="contact-result"></div>
+                            <!-- end of contact result -->
+
+                            <!-- Form Group -->
+                            <div class="form-group">
+                                <input class="form-control border-primary input-box" type="text" name="name" placeholder="Surname First..." autocomplete="off">
+                            </div>
+
+                            <!-- Form Group -->
+                            <div class="form-group">
+                                <input class="form-control input-box" type="email" name="email" placeholder="your-email@purebrass.com" autocomplete="off">
+                            </div>
+
+
+                            <!-- Form Group -->
+                            <div class="form-group">
+                                <span>Date Of Birth</span>
+                                <input class="form-control input-box" type="date" name="subject" placeholder="DOB" autocomplete="off">
+                            </div>
+
+                            <!-- Form Group -->
+                            <div class="form-group">
+                                <input class="form-control input-box" type="text" name="subject" placeholder="Local Government of Origin" autocomplete="off">
+                            </div>
+
+                            <!-- Form Group -->
+                            <div class="form-group">
+                                <input class="form-control input-box" type="text" name="subject" placeholder="Address" autocomplete="off">
+                            </div>
+
+                            <!-- Form Group -->
+                            <div class="form-group">
+                                <input class="form-control input-box" type="text" name="subject" placeholder="NIN" maxlength="11" autocomplete="off">
+                            </div>
+
+                            <!-- Form Group -->
+                            <div class="form-group">
+                            <span>Upload Certificate of Origin(PDF)</span>
+                                <input class="form-control input-box" type="file" name="subject" placeholder="VIN" maxlength="16" autocomplete="off">
+                            </div>
+
+                            
+
+                            <!-- Form Group -->
+                            <div class="form-group text-center">
+                                <button class="btn btn-main btn-effect" type="submit">Submit</button>
+                            </div>
+                        </form>
+                        <!-- End of Contact Form -->
+                    </div>
+
+                <
+
+
+            </div>
+        </section>
+        <!-- =============== END OF SUBSCRIBE SECTION =============== -->
+
+
+<?php include "footer.php"; ?>
